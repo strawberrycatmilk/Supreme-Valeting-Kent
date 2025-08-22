2ba0d67bf9b84924530cceb1446eb4a8518fbccf
funk <68514211+strawberrycatmilk@users.noreply.github.com>
2025-08-22 15:11:51 +0100

    Update server.js
    Changed the redirection link.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 

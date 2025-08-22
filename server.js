40efb14557b329d7051fa5ae97a500aac19a566d
funk <68514211+strawberrycatmilk@users.noreply.github.com>
2025-08-22 15:18:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
     // this sets up the server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 app.get('/api/reviews', (req, res) => {
     try {
-        console.log('We got to the server.')
-        const fs = require('fs')
         const reviewFileAsString = fs.readFileSync('Reviews.csv', 'utf8')
     
         const reviewFileArray = convertToArray(reviewFileAsString)
@@ -38,5 +38,5 @@ function convertToArray(csvString) {
 }
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });

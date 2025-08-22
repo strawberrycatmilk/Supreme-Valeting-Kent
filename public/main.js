a78b131024530f30748c28dbe703e5cd8ae60825
funk <68514211+strawberrycatmilk@users.noreply.github.com>
2025-08-22 14:53:05 +0100

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,40 @@
+// when the window is opened, it calls the backend. 
+window.onload = async () => {
+    try {
+        //  this calls the backend server, to get the reviews.
+        const response = await fetch('http://127.0.0.1:3000/api/reviews');
+        const reviews = await response.json();
+
+        // output all the reviews onto the console.
+        console.log(reviews);
+        
+        for (let i = 0; i < reviews.length; i++) {
+            const reviewHolder = document.createElement("div")
+            reviewHolder.className = "review"
+            document.querySelector(".testimonials").append(reviewHolder)
+
+            for (let j = 0; j < reviews[i][1]; j++) {
+                const stars = document.createElement("img")
+                stars.setAttribute('src', 'Site Files/star-solid-full.svg')
+                stars.setAttribute('height', '25px')
+
+                reviewHolder.append(stars)
+            }
+
+            const name = document.createElement("p")
+            name.innerText = "\t- " + reviews[i][0]
+            reviewHolder.append(name)
+
+            const reviewText = document.createElement("p")
+            review = ""
+            for (let j = 2; j < reviews[i].length; j++) {
+                review += reviews[i][j]
+            }
+            reviewText.innerText = review
+            reviewHolder.append(reviewText)
+        }
+    } catch (error) {
+        console.error('Failed to load reviews:', error);
+    }
+  };
+  
---
+++ server.js
@@ -0,0 +1,39 @@
+    // this sets up the server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+const cors = require('cors');
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/reviews', (req, res) => {
+    try {
+        console.log('We got to the server.')
+        const fs = require('fs')
+        const reviewFileAsString = fs.readFileSync('Reviews.csv', 'utf8')
+    
+        const reviewFileArray = convertToArray(reviewFileAsString)
+        console.log(reviewFileArray)
+        
+        // send back the reviews
+        res.json(reviewFileArray)
+    }
+
+    catch (error) {
+        console.error('Error reading CSV:', error);
+        res.status(500).send('Error reading CSV file');
+    }
+})
+
+function convertToArray(csvString) {
+    return csvString.split(/\r?\n/).map(row => row.split(','));
+}
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });

b1040501a083051784370b1a5d489aff6a0d1b67
funk <68514211+strawberrycatmilk@users.noreply.github.com>
2025-09-04 10:07:31 +0100

    Update reviews.js

--- netlify/functions/reviews.js
+++ netlify/functions/reviews.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
-const path = require('path');
+const fetch = require('node-fetch')
 
 function convertToArray(csvString) {
-  return csvString.split(/\r?\n/).map(row => row.split(','));
+    return csvString.split(/\r?\n/).map(row => row.split(','));
 }
 
-exports.handler = async function(event, context) {
-  try {
-    const filePath = path.join(__dirname, '../../', 'Reviews.csv');
-    const reviewFileAsString = fs.readFileSync(filePath, 'utf8');
-    const reviewFileArray = convertToArray(reviewFileAsString);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(reviewFileArray),
-    };
-  } catch (error) {
-    console.error('Error reading CSV:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ success: false, error: 'Error reading CSV file' }),
-    };
-  }
+exports.handler = async(event, context) => {
+    try {
+        const reviewFileAsString = fs.readFileSync('Reviews.csv', 'utf8')
+    
+        const reviewFileArray = convertToArray(reviewFileAsString)
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify(reviewFileArray)
+        };
+    }
+    
+    catch (error) {
+        console.error('Error reading CSV:', error);
+        return {
+            statusCode: 422, 
+            body: error.stack
+        };
+    }
 };
+
